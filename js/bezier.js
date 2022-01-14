a73532087cfce8f960e9c8aba1763de250ea0c84
Lakasabasz <lakasabasz@gmail.com>
2022-01-14 15:55:44 +0100

    Poprawa beziera

---
+++ js/bezier.js
@@ -0,0 +1,17 @@
+import {MonolitFunction} from './monolitfunction.js';
+
+class Bezier extends MonolitFunction{
+  /**
+  * @param {Object} node1
+  * @param {float} node1.x
+  * @param {float} node1.y
+  * @param {float} node1.d
+  * @param {Object} node2
+  * @param {float} node2.x
+  * @param {float} node2.y
+  * @param {float} node2.d
+  **/
+  constructor(node1, node2){
+    if(node1.)
+  }
+}
